ffa9827299dd747a75b59a0a948ba96de4e30a6e
ajvw2 <129743473+ajvw2@users.noreply.github.com>
2023-06-13 16:14:55 +0200

    Update rock-paper-scissors UI. Implement some functionality

--- script.js
+++ script.js
@@ -19,48 +19,123 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function capitalize(myString) {
-    firstLetter = myString.charAt(0).toUpperCase();
-    rest = myString.substr(1).toLowerCase();
-    return firstLetter + rest;
-}
+// function deletePlayButton() {
+//     this.classList.add('disappear');
+//     playButton.addEventListener('transitionend', () => {return;});
+// }
 
-function game(rounds = 5) {
-    let wins = 0;
-    let computerWins = 0;
-    console.log(`Get ready to play ${rounds} rounds of Rock, Paper, Scissors!`);
-    for (let i = 0; i < rounds; i++) {
-        let choice;
-        while (!choices.includes(choice)) {
-            choice = capitalize(prompt(`Rock, Paper, or Scissors? (Round ${i + 1}/${rounds})`));
-            if (!choices.includes(choice)) {
-                console.log(`Invalid input. Valid inputs: 'rock', 'paper', 'scissors'.`)
-            }
-        }
-        
-        let computerChoice = getComputerChoice();
-        let result = playRound(choice, computerChoice);
- 
-        if (result === 2) {
-            console.log(`You Won! ${choice} beats ${computerChoice}.`);
-            wins++;
-        } else if (result === 1) {
-            console.log(`It's a tie! You both selected ${choice}.`)
-        } else {
-            console.log(`You Lost! ${computerChoice} beats ${choice}.`);
-            computerWins++;
-        }
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+let playerChoice;
+let computerChoice;
+
+const playerScoreDisplay = document.querySelector('.player-score');
+const computerScoreDisplay = document.querySelector('.computer-score');
+const roundDisplay = document.querySelector("#round");
+
+playerScoreDisplay.textContent = `${playerScore}`;
+computerScoreDisplay.textContent = `${computerScore}`;
+roundDisplay.textContent = `ROUND ${round}`;
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const playButton = document.querySelector(".play-button");
+const roundResult = document.querySelector(".round-result");
+
+const computerChoiceDisplay = document.createElement('div');
+computerChoiceDisplay.classList.add('result-text');
+const roundResultDisplay = document.createElement('div');
+roundResultDisplay.classList.add('result-text');
+const roundWinnerDisplay = document.createElement('div');
+roundWinnerDisplay.classList.add('winner-text');
+roundResult.appendChild(computerChoiceDisplay);
+roundResult.appendChild(roundResultDisplay);
+roundResult.appendChild(roundWinnerDisplay);
+
+
+rock.addEventListener('click', () => {
+    playerChoice = 'Rock';
+    rock.classList.add('selected');
+    paper.classList.remove('selected');
+    scissors.classList.remove('selected');
+});
+
+paper.addEventListener('click', () => {
+    playerChoice = 'Paper';
+    paper.classList.add('selected');
+    rock.classList.remove('selected');
+    scissors.classList.remove('selected');
+});
+
+scissors.addEventListener('click', () => {
+    playerChoice = 'Scissors';
+    scissors.classList.add('selected');
+    rock.classList.remove('selected');
+    paper.classList.remove('selected');
+});
+
+
+
+playButton.addEventListener('click', (e) => {
+    if (!playerChoice) {
+        return;
     }
+    // Disable game buttons
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+    playButton.disabled = true;
 
-    console.log(`End result: `);
-    if (wins > computerWins) {
-        console.log(`You won! `);
-    } else if (wins === computerWins) {
-        console.log(`Tied!`);
+    // Delete old result content
+    roundResult.removeChild(computerChoiceDisplay);
+    roundResult.removeChild(roundResultDisplay);
+    roundResult.removeChild(roundWinnerDisplay);
+    
+    // Fade out play button
+    playButton.classList.add('selected');
+    // playButton.addEventListener('transitionend', deletePlayButton);
+
+    computerChoice = getComputerChoice();
+    
+    computerChoiceDisplay.innerHTML = `The computer chose <img class="icon" src="./images/${computerChoice.toLowerCase()}.png">`;
+    roundResult.appendChild(computerChoiceDisplay);
+
+    
+    let result = playRound(playerChoice, computerChoice);
+    if (result == 2) {
+        roundResultDisplay.innerHTML = `<img class="icon" src="./images/${playerChoice.toLowerCase()}.png"> beats <img class="icon" src="./images/${computerChoice.toLowerCase()}.png">`;
+        roundWinnerDisplay.textContent = 'YOU WON!';
+        playerScore++;
+        playerScoreDisplay.textContent = `${playerScore}`;
+    } else if (result == 1) {
+        roundResultDisplay.innerHTML = ' ';
+        roundWinnerDisplay.textContent = 'IT\'S A TIE';
     } else {
-        console.log(`You lost!`);
-    }
-    console.log(`Score: ${wins} vs ${computerWins}.`);
-}
+        roundResultDisplay.innerHTML = `<img class="icon" src="./images/${computerChoice.toLowerCase()}.png"> beats <img class="icon" src="./images/${playerChoice.toLowerCase()}.png">`;
+        roundWinnerDisplay.textContent = 'YOU LOST';
+        computerScore++;
+        computerScoreDisplay.textContent = `${computerScore}`;
+    }  
+    
+    roundResult.appendChild(roundResultDisplay);
+    roundResult.appendChild(roundWinnerDisplay);
 
-game();
+    // Enable and reset buttons
+    rock.disabled = false;
+    rock.classList.remove('selected');
+    paper.disabled = false;
+    paper.classList.remove('selected');
+    scissors.disabled = false;
+    scissors.classList.remove('selected');
+    playButton.disabled = false;
+    playButton.classList.remove('selected');
+
+    // Update round
+    round++;
+    roundDisplay.textContent = `ROUND ${round}`;
+
+});
+
