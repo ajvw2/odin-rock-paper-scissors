15ac608464f360da3c1caa129ad9b8a2fe8c8a53
ajvw2 <129743473+ajvw2@users.noreply.github.com>
2023-06-14 12:56:02 +0200

    Clean up script.js and further implement game functionality

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
-
+// Game functions
 function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
     if (
-        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-        (playerSelection === 'Scissors' && computerSelection === 'Paper)')
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
         )
     {
         return 2;
@@ -19,7 +19,26 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function updateScores(roundResult) {
+    if (roundResult === 2) {
+        playerScore++;
+    } else if (roundResult === 0) {
+        computerScore++;
+    }
+    setScores(playerScore, computerScore);
+}
 
+// UI functions
+function setScores(pScore, cScore) {
+    const playerScoreDisplay = document.querySelector('.score-amount.player');
+    const computerScoreDisplay = document.querySelector('.score-amount.computer');
+    playerScoreDisplay.textContent = `${pScore}`;
+    computerScoreDisplay.textContent = `${cScore}`;
+}
+
+
+
+// Score requirement slider behavior
 const slider = document.querySelector('#score-slider');
 const selectedScore = document.querySelector('#selected-score');
 selectedScore.textContent = `${slider.value}`;
@@ -28,38 +47,58 @@ slider.addEventListener('change', () => {
     selectedScore.textContent = `${slider.value}`;
 });
 
+// Game variables
 let playerScore = 0;
 let computerScore = 0;
 let playerChoice;
 let computerChoice;
+let result;
 
-const playerScoreDisplay = document.querySelector('.score-amount.player');
-const computerScoreDisplay = document.querySelector('.score-amount.computer');
+setScores(playerScore, computerScore);
 
-playerScoreDisplay.textContent = `${playerScore}`;
-computerScoreDisplay.textContent = `${computerScore}`;
+// Game buttons
+const gameButtons = document.querySelectorAll('.game-button');
 
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+// Game execution
+gameButtons.forEach((gameButton) => {
+    if (!gameButton.id.startsWith("computer")) {
+        gameButton.addEventListener('click', () => {
+            playerChoice = `${gameButton.id}`;
+            gameButton.classList.add('selected');
 
-rock.addEventListener('click', () => {
-    playerChoice = 'Rock';
-    rock.classList.add('selected');
-    paper.classList.remove('selected');
-    scissors.classList.remove('selected');
+            // Remove selection from all other buttons
+            gameButtons.forEach((gB) => {
+                if (gB !== gameButton) {
+                    gB.classList.remove('selected');
+                }
+            });
+
+            // Get computer choice
+            computerChoice = getComputerChoice();
+            console.log(computerChoice);
+            gameButtons.forEach((gB) => {
+                if (gB.id.startsWith('computer') && gB.id.endsWith(computerChoice)) {
+                    gB.classList.add('selected');
+                }
+                // Disable all buttons temporarily when choice has been made
+                gB.disabled = true;
+            });
+            
+            // Play round, add to 
+            result = playRound(playerChoice, computerChoice);
+            updateScores(result);
+
+            // Remove selected from all buttons and re-enable
+            gameButtons.forEach((gB) => {
+                gB.classList.remove('selected');
+                gB.disabled = false;
+            })
+        });
+    }  
 });
 
-paper.addEventListener('click', () => {
-    playerChoice = 'Paper';
-    paper.classList.add('selected');
-    rock.classList.remove('selected');
-    scissors.classList.remove('selected');
-});
 
-scissors.addEventListener('click', () => {
-    playerChoice = 'Scissors';
-    scissors.classList.add('selected');
-    rock.classList.remove('selected');
-    paper.classList.remove('selected');
-});
+
+
+
+
